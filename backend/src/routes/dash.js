f8446d4eb6ec1621d8aa6a42722dcff3833c7065
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2025-08-03 09:12:34 +0530

    done

--- backend/src/routes/dash.js
+++ backend/src/routes/dash.js
@@ -1,11 +1,8 @@
 import express from 'express'
 const router = express.Router();
 
-
-
-// Protected route
 router.get('/', (req, res) => {
-  res.json({ msg: 'Welcome to protected route!', user: req.user });
+  res.json({ msg: 'Welcome to Mediraksha', user: req.user });
 });
 
 export default router;
